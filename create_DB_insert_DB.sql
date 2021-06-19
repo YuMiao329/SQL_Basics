4d48571f15af030e8afe81e377b06ca3ac13face
YuMiao329 <ym168@duke.edu>
2021-06-19 19:49:51 +0800

    Update create_DB_insert_DB.sql

--- create_DB_insert_DB.sql
+++ create_DB_insert_DB.sql
@@ -37,3 +37,79 @@ INSERT INTO student(name, major) VALUES('Kate', 'Sociology');
 INSERT INTO student(name, major) VALUES('Claire', 'Undecided');
 INSERT INTO student(name, major) VALUES('Jack', 'Biology');
 INSERT INTO student(name, major) VALUES('Mike', 'Comp Sci');
+-- ---------------------------------------
+-- * Change one Column vairables from all specific value to the other valus
+-- if don't specify WHERE, all variables in that column will be affected
+UPDATE student
+SET major = 'Bio'
+WHERE major = 'Biology';
+-- can also set as (WHERE student_id = 1)
+-- here we merge bio and chemistry major as one Biochemistry major
+UPDATE student
+SET major = 'Biochemistry'
+WHERE major = 'Bio' OR major = 'Chemistry';
+-- here, we will change the first student to Tom with undecided major
+UPDATE student
+SET name = 'Tom', major = 'undecided'
+WHERE student_id = 1;
+-- ---------------------------------------
+-- here we try DELETE: to delete specific row
+DELETE FROM student
+WHERE student_id = 5;
+-- here we specify Tom with undecided major got deleted
+DELETE FROM student
+WHERE name = 'Tom' AND major = 'undecided';
+
+-- ----------------------------------------------
+-- Query Basics
+SELECT * FROM student;
+
+-- here we can try to select *(all) columns from student table 
+SELECT name, major FROM student;
+-- since we have only one table - student, we don't need to 
+-- specify which table is being called
+SELECT student.name, student.major FROM student;
+
+-- we try to order the table by using the alphabetical order from name
+SELECT student.name, student.major FROM student ORDER BY name;
+-- ordering the list with name as descending order
+SELECT student.name, student.major FROM student ORDER BY name DESC;
+SELECT * FROM student ORDER BY student_id DESC;
+SELECT * FROM student ORDER BY student_id ASC;
+-- order two times (first being the main order, second being the second order)
+SELECT * FROM student ORDER BY major, student_id ;
+
+-- LIMIT: only want specific thing/number in the table
+-- here we only show two rows from the table
+SELECT * FROM student LIMIT 2;
+SELECT * FROM student ORDER BY student_id DESC LIMIT 2;
+
+-- filtering:
+-- here only show the student with major is biology
+SELECT * FROM student WHERE major = 'Biology';
+-- here we only keep major is biology or chemistry rows
+SELECT * FROM student WHERE major = 'Biology' or major = 'Chemistry';
+-- here we only keep major is biology or name is Kate rows
+SELECT * FROM student WHERE major = 'Biology' or name = 'Kate';
+
+-- Other useful operators:
+-- <, >, <=, >=, =, <> this is 'not equal' sign, AND, OR
+
+-- here we select rows where major is not equal to Biology majors
+SELECT * FROM student WHERE major <> 'Biology';
+-- here we select rows with student_id is smaller or equal to 3
+SELECT * FROM student WHERE student_id <= 3;
+SELECT * FROM student WHERE student_id <= 3 AND name <> 'Jack';
+
+-- IN statement
+-- we use IN to specify in which group, we select
+-- to extract specific rows with name specified in the parenthesis () 
+SELECT * FROM student
+WHERE name IN ('Claire', 'Kate', 'Mike');
+
+-- say we only interested in the rows with biology and chemistry majors
+SELECT * FROM student
+WHERE major IN ('Biology', 'Chemistry');
+-- with one more constrain: student_id > 2 together
+SELECT * FROM student
+WHERE major IN ('Biology', 'Chemistry') AND student_id > 2;
